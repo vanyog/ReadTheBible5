9b4e43bbe64dd84ec90c7795728497e5d277f255
Vanyo Georgiev <info@vanyog.com>
2026-03-19 17:18:04 +0000

    За успешно създаване на .AppImage файл

--- cpl.sh
+++ cpl.sh
@@ -9,4 +9,4 @@ chmod +x build/rel/usr/bin/Bible5
 cp Bible5.desktop build/rel/
 cp images/Logo_ikon_300x300.png build/rel/Bible5.png
 cp images/Logo_ikon_300x300.png build/rel/usr/share/icons/hicolor/512x512/apps/Bible5.png
-~/Downloads/linuxdeployqt-continuous-x86_64.AppImage build/rel/Bible5.desktop -appimage
+~/Downloads/linuxdeployqt-continuous-$(uname -m).AppImage build/rel/Bible5.desktop -appimage -qmake=/usr/bin/qmake6
