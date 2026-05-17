f0fc47a42070dc25bec602fabcefe425a832b703
Vanyo Georgiev <info@vanyog.com>
2026-05-17 06:31:16 +0000

    Linux arm64 deploy

--- cpl.sh
+++ cpl.sh
@@ -4,9 +4,9 @@ rm -r build/rel
 
 mkdir -p build/rel/usr/bin
 mkdir -p build/rel/usr/share/icons/hicolor/512x512/apps
-cp build/Bible5 build/rel/usr/bin/
-chmod +x build/rel/usr/bin/Bible5
+cp build/ReadBible5 build/rel/usr/bin/
+chmod +x build/rel/usr/bin/ReadBible5
 cp Bible5.desktop build/rel/
-cp images/Logo_ikon_300x300.png build/rel/Bible5.png
-cp images/Logo_ikon_300x300.png build/rel/usr/share/icons/hicolor/512x512/apps/Bible5.png
+cp images/Logo_ikon_300x300.png build/rel/ReadBible5.png
+cp images/Logo_ikon_300x300.png build/rel/usr/share/icons/hicolor/512x512/apps/ReadBible5.png
 ~/Downloads/linuxdeployqt-continuous-$(uname -m).AppImage build/rel/Bible5.desktop -appimage -qmake=/usr/bin/qmake6
